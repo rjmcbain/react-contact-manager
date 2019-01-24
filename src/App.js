8dd342299067524146c6a43b8dddf249b381dee0
rjmcbain <rmcbain01@gmail.com>
2019-01-24 14:55:31 -0700

    adding components

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header branding="Content Manager" />
+        <div className="container">
+          <Contact
+            name="John Doe"
+            email="jdoe@gmail.com"
+            phone="555-555-5555"
+          />
+          <Contact
+            name="Jane Doe"
+            email="janedoe@gmail.com"
+            phone="333-555-5555"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
