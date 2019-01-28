4d89a6aafed238689a19fd208fc226eb52ac882c
rjmcbain <rmcbain01@gmail.com>
2019-01-28 15:34:31 -0700

    working on text input group

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Consumer } from "../context";
+import { Consumer } from "../../context";
 
 class Contact extends Component {
   state = {
     showContactInfo: false
   };
-  onDeleteClick = () => {};
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: "DELETE_CONTACT", payload: id });
+  };
 
   render() {
-    const { name, email, phone } = this.props.contact;
+    const { id, name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
       <Consumer>
         {value => {
+          const { dispatch } = value;
           return (
             <div className="card card-body mb-3">
               <h4>
@@ -31,7 +34,7 @@ class Contact extends Component {
                 <i
                   className="fas fa-times"
                   style={{ cursor: "pointer", float: "right", color: "red" }}
-                  onClick={this.onDeleteClick}
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
                 />
               </h4>
               {showContactInfo ? (
---
+++ src/components/layout/TextInputGroup.js
@@ -0,0 +1,22 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const TextInputGroup = () => {
+  label, name, value, placeholder, type, onChange;
+
+  return (
+    <div className="form-group">
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        className="form-control form-control-lg"
+        placeholder="Enter Name..."
+        value={name}
+        onChange={this.onChange}
+      />
+    </div>
+  );
+};
+
+export default TextInputGroup;
