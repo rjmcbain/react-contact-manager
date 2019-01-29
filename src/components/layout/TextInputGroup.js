e41264e1427e1f6181820265ddc149535d0cf2c1
rjmcbain <rmcbain01@gmail.com>
2019-01-29 13:51:59 -0700

    error checking

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,12 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
-<<<<<<< HEAD
-
-const TextInputGroup = () => {
-  label, name, value, placeholder, type, onChange, error;
-
-=======
 
 const TextInputGroup = ({
   label,
@@ -17,22 +11,11 @@ const TextInputGroup = ({
   onChange,
   error
 }) => {
->>>>>>> 8169a261b3a7cc9f600e29009b76a984b5e33f8f
   return (
     <div className="form-group">
-      <label htmlFor="name">Name</label>
+      <label htmlFor={name}>{label}</label>
       <input
         type={type}
-<<<<<<< HEAD
-        name="name"
-        className={classnames("form-control form-control-lg", {
-          "is-invalid": error
-        })}
-        className="form-control form-control-lg"
-        placeholder="Enter Name..."
-        value={name}
-        onChange={this.onChange}
-=======
         name={name}
         className={classnames("form-control form-control-lg", {
           "is-invalid": error
@@ -40,7 +23,6 @@ const TextInputGroup = ({
         placeholder={placeholder}
         value={value}
         onChange={onChange}
->>>>>>> 8169a261b3a7cc9f600e29009b76a984b5e33f8f
       />
       {error && <div className="invalid-feedback">{error}</div>}
     </div>
@@ -54,11 +36,7 @@ TextInputGroup.propTypes = {
   value: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
-<<<<<<< HEAD
-  error: PropTypes.string.isRequired
-=======
   error: PropTypes.string
->>>>>>> 8169a261b3a7cc9f600e29009b76a984b5e33f8f
 };
 
 TextInputGroup.defaultProps = {
