f0d428c9657dcfc802b37817646dfb6bae17e7b4
rjmcbain <rmcbain01@gmail.com>
2019-01-29 12:24:28 -0700

    error checking

--- src/components/layout/TextInputGroup.js
+++ src/components/layout/TextInputGroup.js
@@ -1,22 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const TextInputGroup = () => {
-  label, name, value, placeholder, type, onChange;
-
+const TextInputGroup = ({
+  label,
+  name,
+  value,
+  placeholder,
+  type,
+  onChange
+}) => {
   return (
     <div className="form-group">
-      <label htmlFor="name">Name</label>
+      <label htmlFor={name}>{label}</label>
       <input
-        type="text"
-        name="name"
+        type={type}
+        name={name}
         className="form-control form-control-lg"
-        placeholder="Enter Name..."
-        value={name}
-        onChange={this.onChange}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
+};
+
+TextInputGroup.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+TextInputGroup.defaultProps = {
+  type: "text"
 };
 
 export default TextInputGroup;
